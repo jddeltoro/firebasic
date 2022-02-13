0e4eb34e7a7b364de450c376da2ad88bb26248c4
jddeltoro <jdiazdeltoro@gmail.com>
2022-02-13 14:41:25 -0300

    add google signin and signout

--- public/app.js
+++ public/app.js
@@ -1,1 +1,33 @@
-console.log(firebase);
+document.addEventListener('DOMContentLoaded', function () {
+    try {
+            let app = firebase.app();
+            let features = [
+                'auth',
+                'database',
+                'firestore',
+                'functions',
+                'messaging',
+                'storage',
+                'analytics',
+                'remoteConfig',
+                'performance',
+            ].filter((feature) => typeof app[feature] === 'function');
+            console.log(`Firebase SDK loaded with ${features.join(', ')}`);
+
+            const auth = firebase.auth();
+
+            const divWhenSignedIn = document.getElementById('whenSignedIn');
+            const divWhenSignedOut = document.getElementById('whenSignedOut');
+            const signInBtn = document.getElementById('signInBtn');
+            const signOutBtn = document.getElementById('signOutBtn');
+            const userDetails = document.getElementById('userDetails');
+
+            const provider = new firebase.auth.GoogleAuthProvider();
+
+            signInBtn.onclick = () => auth.signInWithPopup(provider);
+            signOutBtn.onclick = () => auth.signOut();
+        } catch (e) {
+            console.error(e);
+            loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+        }
+});
